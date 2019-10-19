42e027802b6df760c3627c94b2533f1c76c66a73
071703323fei <55054743+071703323fei@users.noreply.github.com>
2019-10-19 22:53:01 +0800

    Add files via upload
    加第四部分代码

--- wordCount/3.cpp
+++ wordCount/3.cpp
@@ -1,12 +1,23 @@
-#include<iostream>           //加强版，含3 4 5 6 8 9宫 
+#include<iostream>           
 #include<stdlib.h>
 #include<string.h>
 #include<queue>
 #pragma warning(disable : 4996)
 using namespace std;
+struct word
+{
+	char str1[100];
+	int num;
+}A[1000];
+int n=10;
+int X;
+int re;
+char str1[1000][100];
 FILE* fp = NULL;
 FILE* fq = NULL;
 int totalNum[3];
+int t=0;
+void countphrase(char *str);
 int *getCharNum(int *totalNum){
     char buffer[1003];  //缓冲区，存储读取到的每行的内容
     int bufferLen;  // 缓冲区中实际存储的内容的长度
@@ -21,6 +32,7 @@ int *getCharNum(int *totalNum){
     while(fgets(buffer, 1003, fp) != NULL){
         bufferLen = strlen(buffer);
         // 遍历缓冲区的内容
+        countphrase(buffer);
         for(i=0; i<bufferLen; i++){
             c = buffer[i];
             if( c==' ' || c=='\t'){  // 遇到空格
@@ -48,38 +60,138 @@ int *getCharNum(int *totalNum){
     }
     return totalNum;
 }
+
+void print(int n,int t)
+{
+	if(n<t)
+		t=n;
+	for(int i=0;i<t;i++)
+		//cout<<"<"<<A[i].str1<<">"<<A[i].num<<endl; 
+		fprintf(fq,"<%s>: %d\n",A[i].str1,A[i].num);
+}
+void sort1(int t)
+{
+	struct word a;
+	for(int i=0;i<t;i++)
+	{
+		for(int j=i+1;j<t;j++)
+		{
+			if(A[i].num<A[j].num)
+			{
+				a=A[i];A[i]=A[j];A[j]=a;
+			}
+		}
+	}
+}
+void findsame(int n)
+{ 
+	int s[n];
+		for(int i=0;i<n;i++)
+			s[i]=0;
+	for(int i=0;i<n;i++)
+	{
+		if(s[i]==0)
+		{
+			for(int j=0;;j++)
+			{
+				A[t].str1[j]=str1[i][j];
+				if(!str1[i][j])
+				break;
+			}
+			A[t].num++;
+			s[i]=1;
+			for(int m=0;m<n;m++)
+			if(s[m]==0&&strcmp(A[t].str1,str1[m])==0)
+			{
+				A[t].num++;
+				s[m]=1;
+			}
+			t++;
+		}
+	}
+	sort1(t);
+}
+
+
+void countphrase(char *str)
+{
+	int k=0,t=0;
+	int count=0;
+	int N=strlen(str); 
+	for(int i=0;i<N+1;i++)
+	{
+		if(str[i]<='Z'&&str[i]>='A')
+		str[i]=str[i]+32;
+	}
+	for(int i=0;i<N+1;i++)
+	{
+		if((str[i]<='z'&&str[i]>='a')&&count<=4)
+		count++;
+		if(count>4&&str[i]!=' '&&str[i]!='\0')
+		count++;
+		if(count>=4&&(str[i]==' '||str[i]=='\0'))
+		{
+			int m;
+			if(count==4)
+			m=i-count;
+			else
+			m=i-count+1;
+			for(int j=m;j<i;j++)
+			{
+				str1[k][t]=str[j];
+				t++;
+			}
+			k++;
+			t=0;
+			count=0;
+		}
+		if((str[i]==' '||str[i]=='\0')&&count<4)
+		count=0;
+	}
+	findsame(k);
+}
+
 int main(int argc, char* argv[])
 {
 	string inputname;                //命令行输入处理 
 	string outputname;
-	string a, b, c, d;
-	a = "-m";
-	b = "-n";
-	c = "-i";
-	d = "-o";
-	for (int i = 1; i < argc; i++)
+	string a,b,c,d;
+	a="-m";
+	b="-n";
+	c="-i";
+	d="-o";
+	for(int i=1;i<argc;i++)
 	{
-		if (argv[i] == c)
+		if(argv[i]==c)
 		{
 			fp = fopen(argv[++i], "r");     //以文本方式打开文件。
 			if (fp == NULL)               //打开文件出错
 			{
-				cout << "Input not found.\n" << endl;
+				cout<<"Input not found.\n"<<endl; 
 				return 0;
-			}
+			} 
 			continue;
 		}
-		if (argv[i] == d)
+		if(argv[i]==d)
 		{
 			fq = fopen(argv[++i], "w");     //以文本方式打开文件。
 			if (fq == NULL)               //打开文件出错
 			{
-				cout << "Output not found.\n" << endl;
+				cout<<"Output not found.\n"<<endl; 
 				return 0;
-			}
+			} 
 			continue;
 		}
-
+		if(argv[i]==a)
+		{
+			X=argv[++i][0]-'0';
+			continue;
+		}
+		if(argv[i]==b)
+		{
+			re=argv[++i][0]-'0';
+			continue;
+		}
 	}
 	 int totalNum[3] = {0, 0, 0};
 
@@ -91,16 +203,13 @@ int main(int argc, char* argv[])
 	
     if(getCharNum(totalNum)){
     	fprintf(fq,"characters: %d\n",totalNum[1]);
-    	fprintf(fq,"\n");
     	fprintf(fq,"words: %d\n",totalNum[2]);
-    	fprintf(fq,"\n");
     	fprintf(fq,"lines: %d\n",totalNum[0]);
-    	fprintf(fq,"\n");
         }
 		else{
         fprintf(fq,"Error!\n");
     }
-    
+    print(n,t);
 	if (fp != NULL)   fclose(fp);
 	if (fq != NULL)  fclose(fq);
 	return 0;
